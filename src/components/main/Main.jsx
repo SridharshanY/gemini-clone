79a46a2009f07199e37867818a80c8690551b4f8
SridharshanY <sridharshany.2001@gmail.com>
2024-12-14 16:54:41 +0530

    finished for today

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,4 @@
-import { assets } from "../../assets/assets"
+import { assets } from "../../assets/assets";
 
 const Main = () => {
   return (
@@ -9,31 +9,69 @@ const Main = () => {
       </div>
       <div className="max-w-[900px] m-auto">
         <div className="m-12 text-5xl text-gray-300 font-semibold p-5">
-            <p><span className="bg-gradient-to-r from-blue-500 to bg-red-500 bg-clip-text">Hello Dev. </span></p>
-            <p>How can I help you?</p>
+          <p>
+            <span className="bg-gradient-to-r from-blue-500 to bg-red-500 bg-clip-text">
+              Hello Dev.{" "}
+            </span>
+          </p>
+          <p>How can I help you?</p>
         </div>
         <div className="grid grid-flow-col gap-4 p-5">
-            <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
-                <p className="text-gray-600 text-lg">Suggest beautiful places to visit...</p>
-                <img className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3" src={assets.compass_icon} alt="" />
+          <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
+            <p className="text-gray-600 text-lg">
+              Suggest beautiful places to visit...
+            </p>
+            <img
+              className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3"
+              src={assets.compass_icon}
+              alt=""
+            />
+          </div>
+          <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
+            <p className="text-gray-600 text-lg">
+              Briefly summarize a concept...
+            </p>
+            <img
+              className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3"
+              src={assets.bulb_icon}
+              alt=""
+            />
+          </div>
+          <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
+            <p className="text-gray-600 text-lg">
+              Brainstorm team bonding ideas...
+            </p>
+            <img
+              className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3"
+              src={assets.message_icon}
+              alt=""
+            />
+          </div>
+          <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
+            <p className="text-gray-600 text-lg">
+              Improve readability of this code...
+            </p>
+            <img
+              className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3"
+              src={assets.code_icon}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="absolute bottom-0 w-full max-w-[900px] py-0 px-5 m-auto">
+          <div className="flex flex-row items-center justify-between gap-5 bg-gray-300 py-3 px-5 rounded-3xl">
+            <input className="flex-1 bg-transparent outline-none" type="text" placeholder="Ask Gemini" />
+            <div className="flex">
+              <img className="w-6 cursor-pointer" src={assets.gallery_icon} alt="" />
+              <img className="w-6 cursor-pointer" src={assets.mic_icon} alt="" />
+              <img className="w-6 cursor-pointer" src={assets.send_icon} alt="" />
             </div>
-            <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
-                <p className="text-gray-600 text-lg">Briefly summarize a concept...</p>
-                <img className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3" src={assets.bulb_icon} alt="" />
-            </div>
-            <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
-                <p className="text-gray-600 text-lg">Brainstorm team bonding ideas...</p>
-                <img className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3" src={assets.message_icon} alt="" />
-            </div>
-            <div className="h-52 p-4 bg-gray-200 rounded-xl relative cursor-pointer hover:bg-gray-300">
-                <p className="text-gray-600 text-lg">Improve readability of this code...</p>
-                <img className="w-9 p-2 absolute bg-white rounded-3xl bottom-3 right-3" src={assets.code_icon} alt="" />
-            </div>
-
+          </div>
+          <p>Gemini can make mistakes, so double-check it</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
